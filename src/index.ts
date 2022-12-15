8cb4cd1a911db74ef3f73a0b280c4cf909ca2598
giveerr <givessomeerr.e@gmail.com>
2022-12-16 02:45:10 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-console.log('TypeScript Eslint Prettier Starter Template!');
-console.log('A project by Caelin Sutch, follow him at @caelin_sutch');
+console.log('TypeScript Eslint Prettier Starter Template!')
+console.log('A project by Caelin Sutch, follow him at @caelin_sutch')
+
+const sum = (x: number, y: number): number | undefined => {
+  if (x > 5) {
+    return x + y
+  }
+}
+
+console.log(sum(15, 16))
+
+export {}
